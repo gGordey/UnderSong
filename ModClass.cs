e12b566d0839524ddbb9fdcf161138a700bccda0
gGordey <ggordeymini@gmail.com>
2026-06-04 11:34:40 +0300

    init

---
+++ ModClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace UnderSong;
+
+[BepInPlugin("com.silksong.undersong", "UnderSong", "0.0.0")]
+public class ModClass: BaseUnityPlugin
+{
+    public void Awake()
+    {
+        var harmony = new Harmony("com.silksong.undersong");
+        harmony.PatchAll();
+    }
+}
